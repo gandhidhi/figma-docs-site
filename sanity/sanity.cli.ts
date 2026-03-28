2832d97ac27eb38d9318a7592d93336cf5b55169
gandhidhi <yu-suzuki@kua.kyoto-art.ac.jp>
2026-03-28 12:30:33 +0900

    feat: deploy Sanity Studio to figma-docs-site.sanity.studio

---
+++ sanity/sanity.cli.ts
@@ -0,0 +1,12 @@
+import { defineCliConfig } from 'sanity/cli';
+
+export default defineCliConfig({
+  api: {
+    projectId: 'g35v6oh8',
+    dataset: 'production',
+  },
+  studioHost: 'figma-docs-site',
+  deployment: {
+    appId: 'i0kqedty0w4luolzzpl8nexq',
+  },
+});
